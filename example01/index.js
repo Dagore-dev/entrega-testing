cfe1b8ea4752e4fab40d5a8829f7db877cad19d2
Dagore <david.gomez.redondo.dev@gmail.com>
2022-05-21 16:09:57 +0200

    example01

--- example01/index.js
+++ example01/index.js
@@ -1,9 +1,7 @@
 function add (n1, n2) {
-  return n1 + n2
+  return Number(n1) + Number(n2)
 }
 
-function substract (n1, n2) {
-  return n1 - n2
+module.exports = { 
+  add
 }
-
-module.exports = { add, substract }
--- example01/index.test.js
+++ example01/index.test.js
@@ -1,6 +1,49 @@
 const { add } = require('./index.js')
 
-test('2 más 2 es igual a 4', () => {
-  expect(add(2, 2))
-    .toBe(4)
+describe('Probando sumas con la función add', () => {
+  it('2 más 2 es igual a 4', () => {
+    expect(add(2, 2))
+      .toBe(4)
+  })
+  
+  it('2 + -2 es igual a 0', () => {
+    expect(add(2,-2))
+      .toBe(0)
+  })
+
+  it('"2" + 2 es igual a 4', () => {
+    expect(add('2', 2))
+      .toBe(4)
+  })
+
+  it('add(2) es igual a NaN', () => {
+    expect(add(2))
+      .toBe(NaN)
+  })
+
+  it('add(2, 2, 2) es igual a 4', () => {
+    expect(add(2, 2, 2))
+      .toBe(4)
+  })
+
+  it('"hola" + 2 es igual a NaN', () => {
+    expect(add('hola', 2))
+      .toBe(NaN)
+  })
+
+  it('2 + undefined es igual a NaN', () => {
+    expect(add(2, undefined))
+      .toBe(NaN)
+  })
+
+  it('2 + null es igual a 2', () => {
+    expect(add(2, null))
+      .toBe(2)
+  })
+
+  it('null + 2 es igual a 2', () => {
+    expect(add(2, null))
+      .toBe(2)
+  })
+
 })
